fe9f4514f5057b35bcc69e13fef37fd3e0c5634e
WinnerWind <71366717+WinnerWind@users.noreply.github.com>
2025-07-13 12:12:45 +0530

    Allow for colour conversion.
    Now the script is 100% compatible with the original info-cava
    
    Took a hot minute but I'm glad it's done.

--- panel_cava.py
+++ panel_cava.py
@@ -6,6 +6,38 @@ import subprocess
 import sys
 import tempfile
 
+def hex_to_ansi256(hex_color):
+    """
+    Convert a 3-character hex color (like 'fdd') to the closest 256-color ANSI escape code.
+    """
+    if len(hex_color) == 3:
+        # Expand shorthand like 'fdd' → 'ffdd'
+        hex_color = ''.join([c*2 for c in hex_color])
+    try:
+        r = int(hex_color[0:2], 16)
+        g = int(hex_color[2:4], 16)
+        b = int(hex_color[4:6], 16)
+    except ValueError:
+        return ''  # Fallback in case of malformed input
+
+    # Convert RGB to xterm-256 color index
+    def rgb_to_ansi256(r, g, b):
+        # Grayscale range
+        if r == g == b:
+            if r < 8:
+                return 16
+            if r > 248:
+                return 231
+            return round(((r - 8) / 247) * 24) + 232
+        # Color cube range
+        r = round(r / 51)
+        g = round(g / 51)
+        b = round(b / 51)
+        return 16 + (36 * r) + (6 * g) + b
+
+    code = rgb_to_ansi256(r, g, b)
+    return f'\033[38;5;{code}m'
+
 if len(sys.argv) > 1 and sys.argv[1] == '--subproc':
     ramp_list = [' ', '▁', '▂', '▃', '▄', '▅', '▆', '▇', '█']
 
@@ -19,7 +51,7 @@ if len(sys.argv) > 1 and sys.argv[1] == '--subproc':
     reset = '\033[0m'
 
     ramp_list.extend(
-        f'{ansi_colors.get(color.strip(" #"), "")}█{reset}'
+        f'{hex_to_ansi256(color.strip(" #"))}█{reset}'
         for color in sys.argv[2].split(',')
         if color
     )
@@ -78,6 +110,7 @@ def cleanup(sig, frame):
     cava_proc.kill()
     self_proc.kill()
     sys.exit(0)
+
 
 signal.signal(signal.SIGTERM, cleanup)
 signal.signal(signal.SIGINT,  cleanup)
