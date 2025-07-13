09b5469c2b5c146f7dec0456aaba54d111813177
WinnerWind <71366717+WinnerWind@users.noreply.github.com>
2025-07-13 12:37:09 +0530

    Use variables for panel_cava.sh

--- panel_cava.sh
+++ panel_cava.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 export PATH=$HOME/.local/bin:/usr/local/bin:/usr/bin:$PATH
 
-panel_cava.py -f 60 -b 5 -c left > /dev/shm/cava_output_left.txt & 
-panel_cava.py -f 60 -b 5 -c right > /dev/shm/cava_output_right.txt & 
-panel_cava.py -f 60 -b 5 -c average > /dev/shm/cava_output.txt &
+FRAMERATE=60
+BARS=5
+EXTRA_COLORS=fdd,fcc,fbb,faa
+
+panel_cava.py -f $FRAMERATE -b $BARS -c left -e $EXTRA_COLORS > "/dev/shm/cava_output_left.txt" &
+panel_cava.py -f $FRAMERATE -b $BARS -c right -e $EXTRA_COLORS > "/dev/shm/cava_output_right.txt" &
+panel_cava.py -f $FRAMERATE -b $BARS -c average -e $EXTRA_COLORS > "/dev/shm/cava_output.txt" &
+
 wait -n
